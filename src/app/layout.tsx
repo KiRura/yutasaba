c9105f31636adaec148735f779285ec91f091d4e
KiRura <57588049+KiRura@users.noreply.github.com>
2025-07-06 00:02:53 +0900

    metadataを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
+import type { Metadata } from "next";
 import { Header } from "@/components/header";
 import { Provider } from "@/components/ui/provider";
+
+export const metadata: Metadata = {
+	title: "多様化ゆた鯖",
+};
 
 export default function RootLayout(props: { children: React.ReactNode }) {
 	const { children } = props;
